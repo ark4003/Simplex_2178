9c641ff4db44a19d476cd725d8bbe3e54eac56f8
LAPTOP-UPBCNRE5\Andrew <ark4003@g.rit.edu>
2018-07-24 23:29:46 -0400

    E11 DELIVERABLE

--- E11 - EntityManager/MyEntityManager.cpp
+++ E11 - EntityManager/MyEntityManager.cpp
@@ -4,9 +4,18 @@ using namespace Simplex;
 MyEntityManager* MyEntityManager::m_pInstance = nullptr;
 void MyEntityManager::Init(void)
 {
+	//FILL
 }
 void MyEntityManager::Release(void)
 {
+	//FILL
+	for (int i = 0; i < m_uEntityCount; i++)
+	{
+		//Delete all the entities
+	}
+	//there's no entities so make sure the count reflects that
+	m_uEntityCount = 0;
+
 }
 MyEntityManager* MyEntityManager::GetInstance()
 {
@@ -18,6 +27,8 @@ MyEntityManager* MyEntityManager::GetInstance()
 }
 void MyEntityManager::ReleaseInstance()
 {
+	//FILL
+	//delete this instance
 }
 int Simplex::MyEntityManager::GetEntityIndex(String a_sUniqueID)
 {
@@ -50,11 +61,11 @@ matrix4 Simplex::MyEntityManager::GetModelMatrix(String a_sUniqueID)
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, String a_sUniqueID)
 {
-
+	//FILL
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, uint a_uIndex)
 {
-
+	//FILL
 }
 //The big 3
 MyEntityManager::MyEntityManager(){Init();}
@@ -64,19 +75,30 @@ MyEntityManager::~MyEntityManager(){Release();};
 // other methods
 void Simplex::MyEntityManager::Update(void)
 {
-
+	//DONE (I think, I'll know for sure when I fill in the rest)
+	//should probably check for collisions with all the entities
+	for (int i = 0; i < m_uEntityCount - 1; i++)
+	{
+		//check with all the other ones, but NOT the current one
+		//no sense checking collisions with ones that have been checked before either
+		for (int j = i + 1; j < m_uEntityCount; j++)
+		{
+			//check for collision between i and j
+			m_entityList[i]->IsColliding(m_entityList[j]);
+		}
+	}
 }
 void Simplex::MyEntityManager::AddEntity(String a_sFileName, String a_sUniqueID)
 {
-
+	//FILL
 }
 void Simplex::MyEntityManager::RemoveEntity(uint a_uIndex)
 {
-
+	//FILL
 }
 void Simplex::MyEntityManager::RemoveEntity(String a_sUniqueID)
 {
-
+	//FILL
 }
 String Simplex::MyEntityManager::GetUniqueID(uint a_uIndex)
 {
@@ -88,9 +110,9 @@ MyEntity* Simplex::MyEntityManager::GetEntity(uint a_uIndex)
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(uint a_uIndex, bool a_bRigidBody)
 {
-
+	//FILL
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(String a_sUniqueID, bool a_bRigidBody)
 {
-
+	//FILL
 }
